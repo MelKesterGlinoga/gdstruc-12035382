7f36752f340da53eff408517512918e93be885c7
MelKesterGlinoga <melkester.glinoga@benilde.edu.ph>
2021-08-29 14:36:09 +0800

    added sample project

---
+++ sample project/src/com/melkester/Main.java
@@ -0,0 +1,18 @@
+package com.melkester;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
